0e15e9dfc5ee822749d8b0c8376e284cad599da4
mantvmass <kliop2317@gmail.com>
2023-06-01 19:16:18 +0700

    new function for helpers.php

--- src/Illuminati/Foundation/helpers.php
+++ src/Illuminati/Foundation/helpers.php
@@ -21,6 +21,36 @@
         }
     }
 
+    if (!function_exists('basic_render_template')) {
+        /**
+         * This function basic_render_template
+         *
+         * @param  string  $name
+         */
+
+        function basic_render_template(string $name){
+            $templates_path = isset(app::$config["TEMPLATES_PATH"]) ? app::$config["TEMPLATES_PATH"] : "templates/";
+            return require_once(__DIR__ . "/../../../../../../" . $templates_path . $name);
+        }
+    }
+
+    if (!function_exists('jsonify')) {
+        /**
+         * Get an instance of the redirector.
+         *
+         * @param  array  $data
+         * @param  int  $statusCode
+         * @param  string  $contentType
+         */
+
+        function jsonify(array $data, int $statusCode = 200, string $contentType = 'application/json'){
+            http_response_code($statusCode);
+            header('Content-Type: ' . $contentType . '; charset=utf-8');
+            echo json_encode($data);
+            return;
+        }
+    }
+
     if (!function_exists('redirect')) {
         /**
          * Get an instance of the redirector.
@@ -40,8 +70,24 @@
          * @param  int  $statusCode
          * @param  string|null  $message
          */
+
         function abort(int $statusCode, string $message = '') {
             app::handleException($statusCode, $message);
+            return;
+        }
+    }
+
+    if (!function_exists('abortWithJson')) {
+        /**
+         * The abort function.
+         * @param  int $statusCode
+         * @param  array $data
+         */
+
+        function abortWithJson(int $statusCode, array $data) {
+            http_response_code($statusCode);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($data);
             return;
         }
     }
